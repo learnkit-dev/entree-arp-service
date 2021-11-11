b969f15813089f5be3b66173b22e631b1ac48312
Sebastiaan Kloos <sebastiaan@codecycler.com>
2021-11-11 13:19:52 +0100

    Fix action

--- tests/Feature/ArpServiceTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-it('can_connect_to_the_service', function () {
-    $service = new LearnKit\EntreeArpService\EntreeArpService();
-
-    $schools = $service->schoolList();
-
-    expect($schools)->toBeCollection();
-});
